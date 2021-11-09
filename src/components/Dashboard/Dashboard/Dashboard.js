e058f6e0d48b485daee2bab12a9b5115e949613b
musasazib <musasazib@gmail.com>
2021-11-10 02:52:46 +0600

    Added Router

---
+++ src/components/Dashboard/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MakeAdmin from '../MakeAdmin/MakeAdmin';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>This is Dashboard</h1>
+            <MakeAdmin></MakeAdmin>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/components/Dashboard/MakeAdmin/MakeAdmin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MakeAdmin = () => {
+    return (
+        <div>
+            <h2>This is MakeAdmin</h2>
+        </div>
+    );
+};
+
+export default MakeAdmin;
