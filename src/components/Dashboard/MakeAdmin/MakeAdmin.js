ae1cc3181480d9405a2b41c041c34ecb1eecd644
musasazib <musasazib@gmail.com>
2021-11-12 04:24:11 +0600

    Upadate

--- src/components/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/components/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert, Button, Form } from 'react-bootstrap';
 
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+    const [adminSuccess, setAdminSuccess] = useState(false);
+
+    const handleOnBlur = e => {
+        setEmail(e.target.value);
+    }
+
+    const handleAdminSubmit = e => {
+        const user = { email };
+        fetch('http://localhost:5000/users/admin', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    // console.log(data);
+                    setAdminSuccess(true);
+                }
+            })
+        e.preventDefault()
+    }
     return (
         <div>
-            <h2>This is MakeAdmin</h2>
+            <h2>Make an Admin</h2>
+            <Form onSubmit={handleAdminSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                onBlur={handleOnBlur}
+                                name="email"
+                                type="email"
+                                placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+            {/* <form onSubmit={handleAdminSubmit}>
+                <TextField
+                    sx={{ width: '50%' }}
+                    label="Email"
+                    type="email"
+                    onBlur={handleOnBlur}
+                    variant="standard" />
+                <Button type="submit" variant="contained">Make Admin</Button>
+            </form> */}
+            {adminSuccess && <Alert severity="success">Made admin successfully</Alert>}
         </div>
     );
 };
